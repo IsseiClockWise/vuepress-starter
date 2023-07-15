83c22564f87a023910d06410893939a7285db5bb
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-07-15 09:08:27 +0900

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'VuePressの紹介',
+      description: 'ノーコード、無料で専用マニュアルが作れます',
+    }
+  },
+ //HEADの設定は必要に応じて追加して下さい
+  themeConfig: {
+    smoothScroll: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Aパターン', link: '/typeA' },
+      { text: 'ONE', link: '/one.html' },
+      { text: 'TWO', link: '/two.html' },
+    ],
+    sidebar: ['/','/one','/two',['/typeA', 'Aパターン']],　
+  },
+  // ここからもう少し追加します
+}
